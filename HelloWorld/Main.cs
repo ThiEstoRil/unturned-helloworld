06f4c4a09272c3bb1d262202fcf0abb6fc0f86d8
ThiEstoRil <45766139+ThiEstoRil@users.noreply.github.com>
2018-12-18 13:50:25 +0100

    Fixing basic and starter errors

---
+++ HelloWorld/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Rocket.API.DependencyInjection;
+using Rocket.API.Eventing;
+using Rocket.Core.Eventing;
+using Rocket.Core.Player.Events;
+using Rocket.Core.Plugins;
+using Rocket.Core.User;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thiestoril.HelloWorld
+{
+    public class Main : Plugin <Config>
+    {
+        public Main(IDependencyContainer container) : base("Welcome Messager", container);
+
+    }
+}
